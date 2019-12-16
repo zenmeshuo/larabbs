b6f442a17d6d27504456ae6d5243ea1c74b8613f
zihang <95273965@qq.com>
2019-12-16 06:46:01 +0000

    用户可以上传话题图片

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -69,7 +69,17 @@
   <script type="text/javascript">
     $(document).ready(function() {
       var editor = new Simditor({
-        textarea: $('#editor')
+        textarea: $('#editor'),
+        upload: {
+          url: '{{ route('topics.upload_image') }}',
+          params: {
+            _token: '{{ csrf_token() }}'
+          },
+          fileKey: 'upload_file',
+          connectionCount: 3,
+          leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+        },
+        pasteImage: true,
       })
     })
   </script>
